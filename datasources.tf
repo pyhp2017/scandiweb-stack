dbbf050155c14014b0c5ed833cbf897349e74ded
Ahmad Foroughi <pyhp2017@gmail.com>
2023-03-14 16:22:36 +0200

    Add datasources - ubuntu22

---
+++ datasources.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "server_ami_ubuntu_22" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
