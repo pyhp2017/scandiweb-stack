0384f7b016cb58e4d83fc12b635ad9e3e80b8d0e
Ahmad Foroughi <pyhp2017@gmail.com>
2023-03-21 19:55:17 +0200

    Add aws_route53_zone

--- main.tf
+++ main.tf
@@ -37,9 +37,9 @@ resource "aws_subnet" "scandiweb_public_subnet2" {
 
 # Create a subnet for the EC2 instances
 resource "aws_subnet" "scandiweb_instance_subnet" {
-  vpc_id            = aws_vpc.scandiweb_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-central-1a"
+  vpc_id                  = aws_vpc.scandiweb_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true # better to NOT assign public ip to the instances
 
   tags = {
@@ -125,7 +125,7 @@ resource "aws_instance" "scandiweb_magento2" {
   subnet_id              = aws_subnet.scandiweb_instance_subnet.id
   vpc_security_group_ids = [aws_security_group.scandiweb_sg.id]
   key_name               = aws_key_pair.scandiweb_auth.key_name
-  user_data              = "${data.template_file.init.rendered}"
+  user_data              = data.template_file.init.rendered
 
   # Copy the all the magento config files to the instance
   provisioner "file" {
@@ -135,11 +135,11 @@ resource "aws_instance" "scandiweb_magento2" {
 
   # Setup connection to the instance
   connection {
-    type = "ssh"
-    user = "ubuntu"
+    type        = "ssh"
+    user        = "ubuntu"
     private_key = file("~/.ssh/terratest")
-    host = self.public_ip
-    timeout = "4m"
+    host        = self.public_ip
+    timeout     = "4m"
   }
 
   root_block_device {
@@ -159,7 +159,7 @@ resource "aws_instance" "scandiweb_varnish" {
   private_ip             = "10.0.1.61" # Assign a private ip to the instance (Hardcoded for now - needs to be changed)
   vpc_security_group_ids = [aws_security_group.scandiweb_sg.id]
   key_name               = aws_key_pair.scandiweb_auth.key_name
-  user_data              = "${data.template_file.init.rendered}"
+  user_data              = data.template_file.init.rendered
 
   # Copy the default.vcl file to the instance
   provisioner "file" {
@@ -169,11 +169,11 @@ resource "aws_instance" "scandiweb_varnish" {
 
   # Setup connection to the instance
   connection {
-    type = "ssh"
-    user = "ubuntu"
+    type        = "ssh"
+    user        = "ubuntu"
     private_key = file("~/.ssh/terratest")
-    host = self.public_ip
-    timeout = "4m"
+    host        = self.public_ip
+    timeout     = "4m"
   }
 
   root_block_device {
@@ -304,8 +304,25 @@ resource "aws_lb_listener_rule" "scandiweb_lb_listener_rule_magento2" {
 
   condition {
     path_pattern {
-       values = ["/media/*", "/static/*"]
+      values = ["/media/*", "/static/*"]
     }
   }
 }
 
+# Create a resource for domain zone
+resource "aws_route53_zone" "scandiweb_zone" {
+  name = "nowruz.me" # Change this to your domain name
+}
+
+# Create a resource for domain record which will be used to point to the load balancer
+resource "aws_route53_record" "scandiweb_record" {
+  zone_id = aws_route53_zone.scandiweb_zone.zone_id
+  name    = aws_route53_zone.scandiweb_zone.name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.scandiweb_lb.dns_name
+    zone_id                = aws_lb.scandiweb_lb.zone_id
+    evaluate_target_health = false
+  }
+}
